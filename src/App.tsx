e3fab22353c4cd92ac30119fa075fb0f10f35196
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-04-25 15:26:19 +0200

    Update App.tsx
    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from './common/themes/theme';
-import AdminPage from './features/AdminPage/AdminPage';
-import LoginScreen from './features/AdminPage/LoginScreen';
-import EndMessage from './features/EndMessage/EndMessage';
-import StudentInfo from './features/StudentInfo/StudentInfo';
-import TestPage from './features/TestPage/TestPage';
+import LoadingScreen from './common/UIElements/LoadingScreen';
+
+const AdminPage = lazy(() => import('./features/AdminPage/AdminPage'));
+const LoginScreen = lazy(() => import('./features/AdminPage/LoginScreen'));
+const EndMessage = lazy(() => import('./features/EndMessage/EndMessage'));
+const StudentInfo = lazy(() => import('./features/StudentInfo/StudentInfo'));
+const TestPage = lazy(() => import('./features/TestPage/TestPage'));
 
 const Container = styled.div`
   width: auto;
@@ -21,16 +23,18 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Container>
-          <Switch>
-            <Route path="/login" component={LoginScreen} />
-            <Route exact path="/admin" component={AdminPage} />
-            <Route path="/admin/students/:username" component={StudentInfo} />
-            <Route path="/endMessage" component={EndMessage} />
-            <Route path="/" component={TestPage} />
-            <Redirect to="/" />
-          </Switch>
-        </Container>
+        <Suspense fallback={<LoadingScreen message="Loading, please wait" />}>
+          <Container>
+            <Switch>
+              <Route path="/login" component={LoginScreen} />
+              <Route exact path="/admin" component={AdminPage} />
+              <Route path="/admin/students/:username" component={StudentInfo} />
+              <Route path="/endMessage" component={EndMessage} />
+              <Route path="/" component={TestPage} />
+              <Redirect to="/" />
+            </Switch>
+          </Container>
+        </Suspense>
       </BrowserRouter>
     </ThemeProvider>
   );
